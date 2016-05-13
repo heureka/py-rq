f7953441264ff4d1e4c3ad657baf84cc76a411ba
Vladimír Kašpar <vladimir.kaspar@heureka.cz>
2016-05-13 10:59:09 +0200

    Add structure Pool

--- pyrq/helpers.py
+++ pyrq/helpers.py
@@ -4,6 +4,11 @@ def wait_for_synced_slaves(redis, count: int, timeout: int):
         raise NotEnoughSyncedSlavesError('There are only {} synced slaves. Required {}'.format(synced, count))
 
 
+def create_chunks(items, chunk_size):
+    for chunk in [items[i:i + chunk_size] for i in range(0, len(items), chunk_size)]:
+        yield chunk
+
+
 class NotEnoughSyncedSlavesError(Exception):
     pass
 
