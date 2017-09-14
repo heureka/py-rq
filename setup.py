54150ee3eea779a31e64280e777fc403571a3020
Petr Sedivy <petr.sedivy@heureka.cz>
2017-09-14 14:10:11 +0200

    Use hscan_iter to fetch ALL processing queue timeouts (not just first 10). Bump patchnumber.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='py-rq',
-    version='3.0.0',
+    version='3.0.1',
     packages=['pyrq'],
     url='https://github.com/heureka/py-rq',
     license='MIT',
