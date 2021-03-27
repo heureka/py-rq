70f77e375b3c9774bf8c9131c733911bd30fe89c
Vladimír Kašpar <vladimir.kaspar@heureka.cz>
2021-03-27 21:47:47 +0100

    Bump version to 4.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='py-rq',
-    version='3.0.1',
+    version='4.0.0',
     packages=['pyrq'],
     url='https://github.com/heureka/py-rq',
     license='MIT',
